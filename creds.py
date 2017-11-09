b36220cca1a7114d444fc4fb64e3a4220f18f230
Gowtham <gowthambruse@gmail.com>
2017-11-10 02:43:51 +0530

    updated creds

---
+++ creds.py
@@ -0,0 +1,12 @@
+# RENAME THIS FILE TO creds.py before use!
+import os
+
+#Alexa Settings
+ProductID = "whirl_mobile_APP_123"
+Security_Profile_Description = "A standalone app. This includes apps on the web"
+Security_Profile_ID  = "amzn1.application.b77ccabab3f14f91a4821d9956c3098c"
+Client_ID = "amzn1.application-oa2-client.10f009150c2f40708262d3901e383a7f"
+Client_Secret = "d44f77c463852bc28d526b106b2bd92f6bef738ce1c34078c0559dd98b589689"
+
+
+redis_url = "localhost:6379"
